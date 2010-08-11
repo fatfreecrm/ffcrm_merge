5331060768bd9aaafd74e0f469fe33f86b37de76
Nathan B <nathan.f77@gmail.com>
2010-08-11 18:46:01 +0800

    Added ignored logs, finished basic merge logic including model relationships.

--- lib/merge_contacts.rb
+++ lib/merge_contacts.rb
@@ -1,7 +1,45 @@
 module Merge
   module Contacts
-    def merge_with(dup_contact)
-      contact = self
+    # Call this method on the duplicate contact, to merge it
+    # into the master contact.
+    # All attributes from 'self' are default, unless defined in options.
+    def merge_with(master_contact, ignored_attr = [])
+      # Remove ignored attributes from this contact.
+      merge_attr = self.merge_attributes
+      ignored_attr.each do |attr|
+        merge_attr.delete(attr)
+      end
+
+      # Merge class attributes
+      master_contact.update_attributes(merge_attr)
+
+      # Merge 'belongs_to' and 'has_one' associations
+      %w(user lead assignee account business_address).each do |attr|
+        unless ignored_attr.include?(attr)
+          master_contact.send(attr + "=",
+                              self.send(attr))
+        end
+      end
+
+      # Merge 'has_many' associations
+      %w(opportunities tasks activities emails).each do |attr|
+        unless ignored_attr.include?(attr)
+          master_contact.send(attr) << self.send(attr)
+        end
+      end
+
+      master_contact.save!
+      self.destroy!
+    end
+
+    # Defines the list of Contact class attributes we want to merge.
+    def merge_attributes
+      %w(updated_at
+         created_at
+         id).inject(self.attributes) do |r, n|
+          r.delete(n)
+          r
+      end
     end
   end
 end
--- spec/lib/merge_contacts_spec.rb
+++ spec/lib/merge_contacts_spec.rb
@@ -1,6 +1,52 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe Merge::Contacts do
-  it "should have a pending spec"
+describe Contact do
+  before :each do
+    @contact = Factory(:contact)
+    @dup_contact = Factory(:contact)
+    4.times do
+      Factory(:email, :mediator => @contact)
+      Factory(:email, :mediator => @dup_contact)
+    end
+  end
+
+  it "should be able to merge itself into another contact" do
+    # Save the attributes we want to match
+    dup_contact_attr = @dup_contact.merge_attributes
+    dup_contact_emails = @dup_contact.emails
+
+    @dup_contact.merge_with(@contact)
+
+    # Check that the duplicate contact has been merged
+    @contact.merge_attributes.should == dup_contact_attr
+
+    @contact.user.should == @dup_contact.user
+    @contact.lead.should == @dup_contact.lead
+    @contact.account.should == @dup_contact.account
+
+    dup_contact_emails.each do |email|
+      @contact.emails.include?(email).should == true
+    end
+  end
+
+  it "should be able to ignore some attributes when merging" do
+    ignored_attributes = %w(title source background_info)
+
+    # Save the attributes we want to match
+    dup_contact_attr = @dup_contact.merge_attributes
+
+    @dup_contact.merge_with(@contact, %w(title source background_info))
+
+    # Check that the duplicate contact has ignored some attributes
+    ignored_attributes.each do |attr|
+      @contact.send(attr.to_sym).should_not == dup_contact_attr[attr]
+    end
+
+    # Check that other fields have been merged
+    %w(first_name last_name access facebook twitter).each do |attr|
+      @contact.send(attr.to_sym).should == dup_contact_attr[attr]
+    end
+
+  end
 end
 
