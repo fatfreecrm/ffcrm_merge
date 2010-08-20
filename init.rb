e5c9a9ce16018292cce7cb3155c9b27b354d979b
Nathan B <nathan.f77@gmail.com>
2010-08-20 17:44:31 +0800

    Incremented version to 1.2

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ require "fat_free_crm"
 FatFreeCRM::Plugin.register(:crm_merge_contacts, initializer) do
           name "Fat Free Merge Contacts"
         author "Nathan Broadbent"
-       version "1.0"
+       version "1.2"
    description "Basic contact merging"
    dependencies :haml, :simple_column_search
 end
