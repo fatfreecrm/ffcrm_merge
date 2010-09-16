11997a5df1ce54af47830e71d0b37979a481ed1a
Nathan B <nathan.f77@gmail.com>
2010-09-16 17:32:58 +0800

    Instead of overriding the _contact.html.haml partial, we have added a view hook to ffcrm core, and are now calling the _merge_contact_tool.html.haml partial to inject the 'merge' link into each contact div

--- lib/merge/merge_view_hooks.rb
+++ lib/merge/merge_view_hooks.rb
@@ -3,6 +3,10 @@ class MergeViewHooks < FatFreeCRM::Callback::Base
   def javascript_includes(view, context = {})
     view.javascript_include_tag('crm_merge_contacts.js')
   end
+  
+  def contact_tools_before(view, context = {})   
+    view.render(:partial => "/contacts/merge_contact_tool", :locals => {:contact => context[:contact]})
+  end
 
 end
 
