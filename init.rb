c930320b0dfae18e960f0c311f0e562b25281f6d
Nathan B <nathan.f77@gmail.com>
2010-09-30 14:02:04 +0800

    Added test overrides plugin dependecy and factories back into spec helper

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ FatFreeCRM::Plugin.register(:crm_merge_contacts, self) do
         author "Nathan Broadbent"
        version "1.2"
    description "Basic contact merging"
-   dependencies :haml
+   dependencies :haml, :crm_plugin_test_overrides
 end
 
 require "crm_merge_contacts"
