bbd129e05605aed188bdf60bc7d055638a368358
Nathan B <nathan.f77@gmail.com>
2010-12-22 11:00:37 +0800

    Added hooks into overrided partials for contact/account tools. fixed view hooks to load the javascript from vendored plugin

--- lib/crm_merge/merge_view_hooks.rb
+++ lib/crm_merge/merge_view_hooks.rb
@@ -1,9 +1,5 @@
 class MergeViewHooks < FatFreeCRM::Callback::Base
-
-  def javascript_includes(view, context = {})
-    view.javascript_include_tag('crm_merge.js')
-  end
-  
+ 
   def contact_tools_before(view, context = {})   
     view.render(:partial => "/contacts/merge_contact_tool", :locals => {:contact => context[:contact]})
   end
@@ -12,5 +8,9 @@ class MergeViewHooks < FatFreeCRM::Callback::Base
     view.render(:partial => "/accounts/merge_account_tool", :locals => {:account => context[:account]})
   end
 
-end
+  def javascript_epilogue(view, context = {})
+    # Load the crm_merge.js file in the same directory.
+    File.open(File.join(File.dirname(__FILE__),'..','..','public','javascripts','crm_merge.js'), 'r').read
+  end
 
+end
