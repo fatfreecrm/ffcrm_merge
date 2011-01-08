d96d51e8fe4afc17e8a2316a0120bbbe9ce669f6
Nathan B <nathan.f77@gmail.com>
2011-01-08 14:20:16 +0800

    Fixed ignored contacts logic. Was in the wrong place

--- lib/crm_merge/controllers/contacts_controller.rb
+++ lib/crm_merge/controllers/contacts_controller.rb
@@ -50,19 +50,15 @@ ContactsController.class_eval do
   def auto_complete
     @query = params[:auto_complete_query]
     @auto_complete = hook(:auto_complete, self, :query => @query, :user => @current_user)
-    # Filter out ignored contact(s) if param was given.
-    if params[:ignored]
-      ignored_ids = params[:ignored].split(",").map{|i| i.to_i }
-      if @auto_complete[0] # Edge rails 2.3.8 Fat Free CRM
-        @auto_complete[0] = @auto_complete[0].select{|a| !ignored_ids.include?(a.id) }
-      else                 # Release 0.10.1 (@a897de7)
-        @auto_complete = @auto_complete.select{|a| !ignored_ids.include?(a.id) }
-      end
-    end
     if @auto_complete.empty?
       @auto_complete = controller_name.classify.constantize.my(:user => @current_user, :limit => 10).search(@query)
     else
       @auto_complete = @auto_complete.last
+    end
+    # Filter out ignored contact(s) if param was given.
+    if params[:ignored]
+      ignored_ids = params[:ignored].split(",").map{|i| i.to_i }
+      @auto_complete = @auto_complete.select{|a| !ignored_ids.include?(a.id) }
     end
     session[:auto_complete] = controller_name.to_sym
     render :template => "common/auto_complete", :layout => nil
