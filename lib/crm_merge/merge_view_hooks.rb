dab410402c7d040eed0e3bcd72c5367ea3aacb5b
Nathan Broadbent <nathan.f77@gmail.com>
2012-01-03 16:05:54 +0800

    Removed js view hooks to update to new asset pipeline plugin support

--- lib/crm_merge/merge_view_hooks.rb
+++ lib/crm_merge/merge_view_hooks.rb
@@ -1,8 +1,4 @@
 class MergeViewHooks < FatFreeCRM::Callback::Base
-
-  def javascript_includes(view, context = {})
-    view.javascript_include_tag('crm_merge.js')
-  end
   
   def contact_tools_before(view, context = {})   
     view.render(:partial => "/contacts/merge_contact_tool", :locals => {:contact => context[:contact]})
