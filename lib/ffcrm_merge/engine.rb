a3722c657c0c35b99b81914f178a6ad65adb998a
Ben Tillman <ben.tillman@gmail.com>
2012-02-10 15:34:26 +1300

    Add name to initializer so it can be hooked

--- lib/ffcrm_merge/engine.rb
+++ lib/ffcrm_merge/engine.rb
@@ -1,7 +1,8 @@
-module ::FatFreeCrmCrowd
+module ::FatFreeCrmMerge
   class Engine < Rails::Engine
-    config.to_prepare do
+    initializer "ffcrm_merge.boot" do
       require 'find'
+      
       Find.find(File.join(File.dirname(__FILE__), '..', '..', 'app_extensions')) do |file|
         require file if file.end_with?('.rb')
       end
--- lib/ffcrm_merge/merge_view_hooks.rb
+++ lib/ffcrm_merge/merge_view_hooks.rb
@@ -1,12 +1,10 @@
 class MergeViewHooks < FatFreeCRM::Callback::Base
   
   def contact_tools_before(view, context = {})   
-    view.render(:partial => "/contacts/merge_contact_tool", :locals => {:contact => context[:contact]})
+    view.render(:partial => "contacts/merge_tool", :locals => {:contact => context[:contact]})
   end
   
   def account_tools_before(view, context = {})   
-    view.render(:partial => "/accounts/merge_account_tool", :locals => {:account => context[:account]})
+    view.render(:partial => "accounts/merge_tool", :locals => {:account => context[:account]})
   end
-
 end
-
