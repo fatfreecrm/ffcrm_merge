fdac40fd81283dac3696a1a4174e1700fc198518
Steve Kenworthy <steveyken@gmail.com>
2013-02-07 16:04:16 +0800

    Separated merge helpers into entity files.

---
+++ app/helpers/accounts_merge_helper.rb
@@ -0,0 +1,50 @@
+module AccountsMergeHelper
+
+  # Transforms the list of merge attributes into a display
+  # format (ie, with links / model associations), to be
+  # displayed in the merge selection table.
+  # --------------------------------------------------------
+  def account_merge_attributes(account, html = true)
+    attr_hash = account.merge_attributes
+    # ------ Humanize attributes
+    attr_hash['source'] = attr_hash['source'].humanize if attr_hash['source']
+    # ------ Make websites into links and emails into mailto links
+    if html
+      %w(blog linkedin facebook twitter).each do |attribute|
+        if link = attr_hash[attribute] and not link.blank?
+          attr_hash[attribute] = link_to_new_window(link, link)
+        end
+      end
+      %w(email alt_email).each do |attribute|
+        if email = attr_hash[attribute] and not email.blank?
+          attr_hash[attribute] = mail_to(email)
+        end
+      end
+    end
+    # ------ Boolean values should be yes/no
+    attr_hash['do_not_call'] = attr_hash['do_not_call'] == true ? "Yes" : "No"
+    # ------ User relationships should display user's full name
+    %w(assigned_to reports_to user_id).each do |attribute|
+      if attr_hash[attribute]
+        user = User.find(attr_hash[attribute])
+        attr_hash[attribute] = html ? link_to_new_window(user.full_name, user_path(user)) : user.full_name
+      end
+    end
+    # ------ Lead relationship should display lead's full name
+    if attr_hash['lead_id']
+      lead = Lead.find(attr_hash['lead_id'])
+      attr_hash['lead_id'] = html ? link_to_new_window(lead.full_name, lead_path(lead)) : lead.full_name
+    end
+    
+    # ------ Addresses should display in full
+    account.address_attributes.keys.each do |attribute|
+      if attr_hash[attribute]
+        address_type = attribute.gsub('_address', '')
+        attr_hash[attribute] = render("shared/address_show", :asset => account, :type => address_type)
+      end
+    end
+
+    attr_hash
+  end
+
+end
